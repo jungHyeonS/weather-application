a2bd2da7c9d7415b3a0db4beba800966006d3bbf
jungHyeonS <fbtls25@naver.com>
2022-06-12 12:15:24 +0900

    로딩 컴포넌트 추가 및 날씨 정보 바인딩

---
+++ src/components/incLoading.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="loading">
+        <i class="fas fa-circle-notch fa-spin"></i>
+        <p>날씨 정보를 불러오고 있습니다</p>
+    </div>
+</template>
+<script>
+export default {
+    name : "loadingCom"
+}
+</script>
--- src/pages/main.vue
+++ src/pages/main.vue
@@ -1,31 +1,35 @@
 <template>
-    <div class="main">
+    <div class="inc" v-if="isLoading">
+        <loading></loading>
+    </div>
+    <div class="main" v-if="!isLoading">
         <div class="topInfo">
-            <p class="time">13:20</p>
+            <!--  -->
+            <p class="time">{{getTime(weatherData.current.dt)}}</p>
             <h1 class="area">광진구 능동</h1>
             <img src="@/assets/img/icons/w1.png"/>
-            <h1 class="temperature">31도</h1>
+            <h1 class="temperature">{{pad(weatherData.current.temp)}}도</h1>
             <p class="otherTemperature">어제보다 1도 낮아요</p>
         </div>
         <div class="otherInfo">
             <div class="fl">
                 <div>
-                    <span class="title">미세먼지</span>
-                    <span>나쁨</span>
+                    <span class="title">체감온도</span>
+                    <span>{{pad(weatherData.current.feels_like)}}도</span>
                 </div>
                 <div>
-                    <span class="title">강수확률</span>
-                    <span>40%</span>
+                    <span class="title">자외선 지수</span>
+                    <span>{{covertUVI(weatherData.current.uvi)}}</span>
                 </div>
             </div>
             <div class="fl">
                 <div>
                     <span class="title">습도</span>
-                    <span>나쁨</span>
+                    <span>{{weatherData.current.humidity}}%</span>
                 </div>
                 <div>
                     <span class="title">풍속</span>
-                    <span>1.3m/s</span>
+                    <span>{{weatherData.current.wind_speed}}m/s</span>
                 </div>
             </div>
         </div>
@@ -49,18 +53,67 @@
 import timeInfo from "@/components/timeInfo.vue"
 import weekInfo from "@/components/weekInfo.vue"
 import mapInfo from "@/components/mapInfo.vue";
+import loading from "@/components/incLoading.vue"
+
 import { ref,onMounted,inject, reactive} from "vue";
+
 export default {
     name : "mainPage",
     components:{
         timeInfo,
         weekInfo,
-        mapInfo
+        mapInfo,
+        loading
+    },
+    computed:{
+        covertUVI(){
+            return(uvi) => {
+                let result = "";
+                if(uvi >= 9.0){
+                    result = "매우 높음"
+                }else if(uvi >= 7.0){
+                    result = "높음"
+                }else if(uvi >= 5.0){
+                    result = "보통"
+                }else if(uvi >= 3.0){
+                    result = "낮음"
+                }else{
+                    result = "매우 낮음"
+                }
+                return result
+            }
+        },
+        pad(){
+            return (temp) => {
+                return Number(temp).toFixed(0)
+            }
+        },
+        getTime(){
+            return (ms) => {
+                let seconds = Math.floor(ms / 1000);
+                let minutes = Math.floor(seconds / 60);
+                let hours = Math.floor(minutes / 60);
+
+                seconds = seconds % 60;
+                minutes = seconds >= 30 ? minutes + 1 : minutes;
+
+                minutes = minutes % 60;
+                hours = hours % 24;
+                return `${hours}:${minutes}`;
+            }
+        }
     },
     setup(){
+        const isLoading = ref(true);
         const axios = inject('axios');
         const key = "1523955ec9313db7ddcf2463f1dcf67c"
+        
+        let weatherData = reactive(
+            {current : null}
+        )
         let isActive = ref(1)
+
+
         const changeNav = (tab) => {
             isActive.value = tab;
         }
@@ -80,8 +133,9 @@ export default {
 
         const weather = reactive({
             getWeather : (lat,long) =>{
-                axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&appid=${key}&lang=kr`).then((res)=>{
-                    console.log(res);
+                axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&appid=${key}&lang=kr&units=metric`).then((res)=>{
+                    weatherData.current = res.data.current
+                    isLoading.value = false
                 }).catch((err)=>{
                     console.log(err);
                 });
@@ -91,7 +145,7 @@ export default {
         onMounted(() => {
             getGeoLocation()
         })
-        return {isActive,changeNav,getGeoLocation,weather}
+        return {isActive,changeNav,getGeoLocation,weather,weatherData,isLoading}
     }
 }
 </script>
