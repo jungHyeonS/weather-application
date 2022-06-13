99a60f6eb28bc040d540578e4af57a4267ca3264
jungHyeonS <fbtls25@naver.com>
2022-06-13 22:57:42 +0900

    vuex 적용 및 변수 로직 정리

--- src/pages/main.vue
+++ src/pages/main.vue
@@ -1,50 +1,50 @@
 <template>
-    <div class="inc" v-if="isLoading">
+    <div class="inc" v-if="inc.isLoading">
         <loading></loading>
     </div>
-    <div class="main" v-if="!isLoading">
+    <div class="main" v-if="!inc.isLoading">
         <div class="topInfo">
             <!--  -->
-            <p class="time">{{getTime(weatherData.current.dt)}}</p>
+            <p class="time">{{getTime(weather.weatherData.current.dt)}}</p>
             <h1 class="area">광진구 능동</h1>
             <img src="@/assets/img/icons/w1.png"/>
-            <h1 class="temperature">{{pad(weatherData.current.temp)}}도</h1>
+            <h1 class="temperature">{{pad(weather.weatherData.current.temp)}}도</h1>
             <p class="otherTemperature">어제보다 1도 낮아요</p>
         </div>
         <div class="otherInfo">
             <div class="fl">
                 <div>
                     <span class="title">체감온도</span>
-                    <span>{{pad(weatherData.current.feels_like)}}도</span>
+                    <span>{{pad(weather.weatherData.current.feels_like)}}도</span>
                 </div>
                 <div>
                     <span class="title">자외선 지수</span>
-                    <span>{{covertUVI(weatherData.current.uvi)}}</span>
+                    <span>{{store.state.uvi}}</span>
                 </div>
             </div>
             <div class="fl">
                 <div>
                     <span class="title">습도</span>
-                    <span>{{weatherData.current.humidity}}%</span>
+                    <span>{{weather.weatherData.current.humidity}}%</span>
                 </div>
                 <div>
                     <span class="title">풍속</span>
-                    <span>{{weatherData.current.wind_speed}}m/s</span>
+                    <span>{{weather.weatherData.current.wind_speed}}m/s</span>
                 </div>
             </div>
         </div>
         <div class="bottomInfo">
             <div class="bottomNav">
                 <ul>
-                    <li :class="{'active' : isActive == 1}" @click="changeNav(1)">시간대별</li>
-                    <li :class="{'active' : isActive == 2}"  @click="changeNav(2)">주간예보</li>
-                    <li :class="{'active' : isActive == 3}"  @click="changeNav(3)">전국날씨</li>
+                    <li :class="{'active' : inc.isActive == 1}" @click="inc.changeNav(1)">시간대별</li>
+                    <li :class="{'active' : inc.isActive == 2}"  @click="inc.changeNav(2)">주간예보</li>
+                    <li :class="{'active' : inc.isActive == 3}"  @click="inc.changeNav(3)">전국날씨</li>
                 </ul>
             </div>
             <div class="bottom">
-                <timeInfo v-if="isActive == 1"></timeInfo>
-                <weekInfo v-if="isActive == 2"></weekInfo>
-                <mapInfo v-if="isActive == 3"></mapInfo>
+                <timeInfo v-if="inc.isActive == 1"></timeInfo>
+                <weekInfo v-if="inc.isActive == 2"></weekInfo>
+                <mapInfo v-if="inc.isActive == 3"></mapInfo>
             </div>
         </div>
     </div>
@@ -55,7 +55,9 @@ import weekInfo from "@/components/weekInfo.vue"
 import mapInfo from "@/components/mapInfo.vue";
 import loading from "@/components/incLoading.vue"
 
-import { ref,onMounted,inject, reactive} from "vue";
+import { useStore } from "vuex";
+
+import {onMounted,inject, reactive} from "vue";
 
 export default {
     name : "mainPage",
@@ -66,23 +68,6 @@ export default {
         loading
     },
     computed:{
-        covertUVI(){
-            return(uvi) => {
-                let result = "";
-                if(uvi >= 9.0){
-                    result = "매우 높음"
-                }else if(uvi >= 7.0){
-                    result = "높음"
-                }else if(uvi >= 5.0){
-                    result = "보통"
-                }else if(uvi >= 3.0){
-                    result = "낮음"
-                }else{
-                    result = "매우 낮음"
-                }
-                return result
-            }
-        },
         pad(){
             return (temp) => {
                 return Number(temp).toFixed(0)
@@ -104,48 +89,53 @@ export default {
         }
     },
     setup(){
-        const isLoading = ref(true);
+        const store = useStore();
         const axios = inject('axios');
         const key = "1523955ec9313db7ddcf2463f1dcf67c"
-        
-        let weatherData = reactive(
-            {current : null}
-        )
-        let isActive = ref(1)
-
-
-        const changeNav = (tab) => {
-            isActive.value = tab;
-        }
-
-
-        const getGeoLocation = () => {
-            if(navigator.geolocation){
-                navigator.geolocation.getCurrentPosition((pos)=>{
-                    weather.getWeather(pos.coords.latitude,pos.coords.longitude)
-                },(err)=>{
-                    console.log(err);
-                })
-            }else{
-                alert("GPS를 지원하지 않습니다.")
+     
+        const inc = reactive({
+            isActive : 1,
+            isLoading : true,
+            changeNav : (tab)=>{
+                inc.isActive = tab
             }
-        }
+        })
+
 
         const weather = reactive({
+            weatherData :{
+                current : null
+            },
             getWeather : (lat,long) =>{
                 axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&appid=${key}&lang=kr&units=metric`).then((res)=>{
-                    weatherData.current = res.data.current
-                    isLoading.value = false
+                    weather.weatherData.current = res.data.current
+                    store.commit("covertUVI",weather.weatherData.current.uvi)
+                    inc.isLoading = false
                 }).catch((err)=>{
                     console.log(err);
                 });
+            },
+            getGeoLocation : () =>{
+                if(navigator.geolocation){
+                    navigator.geolocation.getCurrentPosition((pos)=>{
+                        weather.getWeather(pos.coords.latitude,pos.coords.longitude)
+                    },(err)=>{
+                        console.log(err);
+                    })
+                }else{
+                    alert("GPS를 지원하지 않습니다.")
+                }
             }
         })
 
         onMounted(() => {
-            getGeoLocation()
+            weather.getGeoLocation()
         })
-        return {isActive,changeNav,getGeoLocation,weather,weatherData,isLoading}
+        return {
+            inc,
+            weather,
+            store
+        }
     }
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,31 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    uvi:"",
+    pad:0,
+    time:""
   },
   getters: {
+    getUVI(state){
+      return state.uvi
+    }
   },
   mutations: {
+    covertUVI(state,value){
+      let result = ""
+      if(value >= 9.0){
+        result = "매우 높음"
+      }else if(value >= 7.0){
+          result = "높음"
+      }else if(value >= 5.0){
+          result = "보통"
+      }else if(value >= 3.0){
+          result = "낮음"
+      }else{
+          result = "매우 낮음"
+      }
+      state.uvi = result;
+    }
   },
   actions: {
   },
